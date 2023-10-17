786fd6c3791b1ca60b11c9741929e9431c16b065
konka <1094918@gmail.com>
2023-10-17 22:27:12 +0800

    fix filename and button missing

--- src/content/postDetail.ts
+++ src/content/postDetail.ts
@@ -2,125 +2,123 @@ import dayjs from 'dayjs';
 import { downloadResource, getMediaName, getUrlFromInfoApi, openInNewTab } from './utils';
 
 function postGetArticleNode(target: HTMLAnchorElement) {
-	let articleNode: HTMLElement = target;
-	while (articleNode.tagName !== 'ARTICLE') {
-		articleNode = articleNode.parentNode as HTMLElement;
-	}
-	return articleNode;
+   let articleNode: HTMLElement = target;
+   while (articleNode.tagName !== 'ARTICLE') {
+      articleNode = articleNode.parentNode as HTMLElement;
+   }
+   return articleNode;
 }
 
 async function fetchVideoURL(articleNode: HTMLElement, videoElem: HTMLVideoElement) {
-	const poster = videoElem.getAttribute('poster');
-	const timeNodes = articleNode.querySelectorAll('time');
-	const posterUrl = (timeNodes[timeNodes.length - 1].parentNode!.parentNode as any).href;
-	const posterPattern = /\/([^\/?]*)\?/;
-	const posterMatch = poster?.match(posterPattern);
-	const postFileName = posterMatch?.[1];
-	const resp = await fetch(posterUrl);
-	const content = await resp.text();
-	const pattern = new RegExp(`${postFileName}.*?video_versions.*?url":("[^"]*")`, 's');
-	const match = content.match(pattern);
-	let videoUrl = JSON.parse(match?.[1] ?? '');
-	videoUrl = videoUrl.replace(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/g, 'https://scontent.cdninstagram.com');
-	videoElem.setAttribute('videoURL', videoUrl);
-	return videoUrl;
+   const poster = videoElem.getAttribute('poster');
+   const timeNodes = articleNode.querySelectorAll('time');
+   const posterUrl = (timeNodes[timeNodes.length - 1].parentNode!.parentNode as any).href;
+   const posterPattern = /\/([^\/?]*)\?/;
+   const posterMatch = poster?.match(posterPattern);
+   const postFileName = posterMatch?.[1];
+   const resp = await fetch(posterUrl);
+   const content = await resp.text();
+   const pattern = new RegExp(`${postFileName}.*?video_versions.*?url":("[^"]*")`, 's');
+   const match = content.match(pattern);
+   let videoUrl = JSON.parse(match?.[1] ?? '');
+   videoUrl = videoUrl.replace(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/g, 'https://scontent.cdninstagram.com');
+   videoElem.setAttribute('videoURL', videoUrl);
+   return videoUrl;
 }
 
 const getVideoSrc = async (articleNode: HTMLElement, videoElem: HTMLVideoElement) => {
-	let url = videoElem.getAttribute('src');
-	if (videoElem.hasAttribute('videoURL')) {
-		url = videoElem.getAttribute('videoURL');
-	} else if (url === null || url.includes('blob')) {
-		url = await fetchVideoURL(articleNode, videoElem);
-	}
-	return url;
+   let url = videoElem.getAttribute('src');
+   if (videoElem.hasAttribute('videoURL')) {
+      url = videoElem.getAttribute('videoURL');
+   } else if (url === null || url.includes('blob')) {
+      url = await fetchVideoURL(articleNode, videoElem);
+   }
+   return url;
 };
 
 async function getUrl() {
-	const articleNode = document.querySelector('section main') as HTMLElement;
-	if (!articleNode) return;
-	const list = articleNode.querySelectorAll('li[style][class]');
-	let url: string | null = null;
-	if (list.length === 0) {
-		// single img or video
-		url = await getUrlFromInfoApi(articleNode);
+   const articleNode = document.querySelector('section main') as HTMLElement;
+   if (!articleNode) return;
+   const list = articleNode.querySelectorAll('li[style][class]');
+   let url: string | null = null;
+   if (list.length === 0) {
+      // single img or video
+      url = await getUrlFromInfoApi(articleNode);
 
-		if (url === null) {
-			const videoElem: HTMLVideoElement | null = articleNode.querySelector('article  div > video');
-			const imgElem = articleNode.querySelector('article  div[role] div > img');
-			if (videoElem) {
-				// media type is video
-				if (videoElem) {
-					url = await getVideoSrc(articleNode, videoElem);
-				}
-			} else if (imgElem) {
-				// media type is image
-				url = imgElem.getAttribute('src');
-			} else {
-				console.log('Err: not find media at handle post single');
-			}
-		}
-	} else {
-		// multiple imgs or videos
-		const dotsList = articleNode.querySelectorAll(`:scope > div > div > div > div>div>div>div>div>div>div:nth-of-type(2)>div`);
-		const mediaIndex = [...dotsList].findIndex((i) => i.classList.length === 2);
-		url = await getUrlFromInfoApi(articleNode, mediaIndex);
-		if (url === null) {
-			const listElements = [
-				...articleNode.querySelectorAll(`:scope > div > div:nth-child(1) > div > div:nth-child(1) ul li[style*="translateX"]`),
-			] as HTMLLIElement[];
-			const listElementWidth = Math.max(...listElements.map((element) => element.clientWidth));
-			const positionsMap: Record<string, HTMLLIElement> = listElements.reduce((result, element) => {
-				const position = Math.round(Number(element.style.transform.match(/-?(\d+)/)?.[1]) / listElementWidth);
-				return { ...result, [position]: element };
-			}, {});
+      if (url === null) {
+         const videoElem: HTMLVideoElement | null = articleNode.querySelector('article  div > video');
+         const imgElem = articleNode.querySelector('article  div[role] div > img');
+         if (videoElem) {
+            // media type is video
+            if (videoElem) {
+               url = await getVideoSrc(articleNode, videoElem);
+            }
+         } else if (imgElem) {
+            // media type is image
+            url = imgElem.getAttribute('src');
+         } else {
+            console.log('Err: not find media at handle post single');
+         }
+      }
+   } else {
+      // multiple imgs or videos
+      const dotsList = articleNode.querySelectorAll(`:scope > div > div > div > div>div>div>div>div>div>div:nth-of-type(2)>div`);
+      const mediaIndex = [...dotsList].findIndex((i) => i.classList.length === 2);
+      url = await getUrlFromInfoApi(articleNode, mediaIndex);
+      if (url === null) {
+         const listElements = [
+            ...articleNode.querySelectorAll(`:scope > div > div:nth-child(1) > div > div:nth-child(1) ul li[style*="translateX"]`),
+         ] as HTMLLIElement[];
+         const listElementWidth = Math.max(...listElements.map((element) => element.clientWidth));
+         const positionsMap: Record<string, HTMLLIElement> = listElements.reduce((result, element) => {
+            const position = Math.round(Number(element.style.transform.match(/-?(\d+)/)?.[1]) / listElementWidth);
+            return { ...result, [position]: element };
+         }, {});
 
-			const node = positionsMap[mediaIndex];
-			const videoElem = node.querySelector('video');
-			const imgElem = node.querySelector('img');
-			if (videoElem) {
-				// media type is video
-				url = await getVideoSrc(articleNode, videoElem);
-			} else if (imgElem) {
-				// media type is image
-				url = imgElem.getAttribute('src');
-			}
-		}
-	}
-	return url;
+         const node = positionsMap[mediaIndex];
+         const videoElem = node.querySelector('video');
+         const imgElem = node.querySelector('img');
+         if (videoElem) {
+            // media type is video
+            url = await getVideoSrc(articleNode, videoElem);
+         } else if (imgElem) {
+            // media type is image
+            url = imgElem.getAttribute('src');
+         }
+      }
+   }
+   return url;
 }
 
 export async function postDetailOnClicked(target: HTMLAnchorElement) {
-	try {
-		const url = await getUrl();
-		console.log('url', url);
-		if (url && url.length > 0) {
-			if (target.className.includes('download-btn')) {
-				try {
-					const tagNode = document.querySelector(
-						'path[d="M21.334 23H2.666a1 1 0 0 1-1-1v-1.354a6.279 6.279 0 0 1 6.272-6.272h8.124a6.279 6.279 0 0 1 6.271 6.271V22a1 1 0 0 1-1 1ZM12 13.269a6 6 0 1 1 6-6 6.007 6.007 0 0 1-6 6Z"]'
-					);
-					const postTime = document.querySelector('time')?.getAttribute('datetime');
-					const profileName = document.querySelector('header section')?.querySelector('a')?.innerText;
-					let posterName = profileName ?? document.querySelector('header')!.querySelector('a')!.getAttribute('href')!.replace(/\//g, '');
-					if (tagNode) {
-						const avatar = document.querySelector('canvas')?.nextElementSibling;
-						if (avatar instanceof HTMLAnchorElement) {
-							posterName = avatar.getAttribute('href')!.replace(/\//g, '');
-						}
-					}
-
-					downloadResource(url, posterName + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url));
-				} catch (e) {
-					downloadResource(url);
-				}
-			} else {
-				openInNewTab(url);
-			}
-		}
-	} catch (e: any) {
-		alert('Download Failed!');
-		console.log(`Uncatched in postDetailOnClicked(): ${e}\n${e.stack}`);
-		return null;
-	}
+   try {
+      const url = await getUrl();
+      console.log('url', url);
+      if (url && url.length > 0) {
+         if (target.className.includes('download-btn')) {
+            const tagNode = document.querySelector(
+               'path[d="M21.334 23H2.666a1 1 0 0 1-1-1v-1.354a6.279 6.279 0 0 1 6.272-6.272h8.124a6.279 6.279 0 0 1 6.271 6.271V22a1 1 0 0 1-1 1ZM12 13.269a6 6 0 1 1 6-6 6.007 6.007 0 0 1-6 6Z"]'
+            );
+            const postTime = document.querySelector('time')?.getAttribute('datetime');
+            let posterName = document.querySelector('header section')?.querySelector('a')?.innerText;
+            if (!posterName) {
+               const nameLink = document.querySelector('main a[role="link"]');
+               if (nameLink instanceof HTMLAnchorElement) posterName = nameLink.innerText;
+            }
+            if (tagNode) {
+               const avatar = document.querySelector('canvas')?.nextElementSibling;
+               if (avatar instanceof HTMLAnchorElement) {
+                  posterName = avatar.getAttribute('href')?.replace(/\//g, '');
+               }
+            }
+            downloadResource(url, posterName ? posterName + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url) : '');
+         } else {
+            openInNewTab(url);
+         }
+      }
+   } catch (e: any) {
+      alert('Download Failed!');
+      console.log(`Uncatched in postDetailOnClicked(): ${e}\n${e.stack}`);
+      return null;
+   }
 }
--- src/content/stories.ts
+++ src/content/stories.ts
@@ -1,90 +1,84 @@
 import dayjs from 'dayjs';
-import {downloadResource, getMediaName, getUrlFromInfoApi, openInNewTab} from './utils';
+import { downloadResource, getMediaName, getUrlFromInfoApi, openInNewTab } from './utils';
 
 function storyGetSectionNode(target: HTMLAnchorElement) {
-    let sectionNode: HTMLElement = target;
-    while (sectionNode && sectionNode.tagName !== 'SECTION') {
-        sectionNode = sectionNode.parentNode as HTMLElement;
-    }
-    return sectionNode;
+   let sectionNode: HTMLElement = target;
+   while (sectionNode && sectionNode.tagName !== 'SECTION') {
+      sectionNode = sectionNode.parentNode as HTMLElement;
+   }
+   return sectionNode;
 }
 
 async function storyGetUrl(target: HTMLElement, sectionNode: any) {
-    let url: string | null = null;
+   let url: string | null = null;
 
-    url = await getUrlFromInfoApi(target);
+   url = await getUrlFromInfoApi(target);
 
-    if (!url) {
-        if (sectionNode.querySelector('video > source')) {
-            url = sectionNode.querySelector('video > source').getAttribute('src');
-        } else if (sectionNode.querySelector('img[decoding="sync"]')) {
-            const img = sectionNode.querySelector('img[decoding="sync"]');
-            url = img.srcset.split(/ \d+w/g)[0].trim(); // extract first src from srcset attr. of img
-            if (url && url.length > 0) {
-                return url;
-            }
-            url = sectionNode.querySelector('img[decoding="sync"]').getAttribute('src');
-        } else if (sectionNode.querySelector('video')) {
-            url = sectionNode.querySelector('video').getAttribute('src');
-        }
-    }
-    return url;
+   if (!url) {
+      if (sectionNode.querySelector('video > source')) {
+         url = sectionNode.querySelector('video > source').getAttribute('src');
+      } else if (sectionNode.querySelector('img[decoding="sync"]')) {
+         const img = sectionNode.querySelector('img[decoding="sync"]');
+         url = img.srcset.split(/ \d+w/g)[0].trim(); // extract first src from srcset attr. of img
+         if (url && url.length > 0) {
+            return url;
+         }
+         url = sectionNode.querySelector('img[decoding="sync"]').getAttribute('src');
+      } else if (sectionNode.querySelector('video')) {
+         url = sectionNode.querySelector('video').getAttribute('src');
+      }
+   }
+   return url;
 }
 
 export async function storyOnClicked(target: HTMLAnchorElement) {
-    const sectionNode = storyGetSectionNode(target);
-    const pathname = window.location.pathname
-    const pathnameArr = pathname.split('/')
-    const posterName = pathnameArr[2];
-    const postTime = sectionNode.querySelector('time')?.getAttribute('datetime');
+   const sectionNode = storyGetSectionNode(target);
+   const pathname = window.location.pathname;
+   const pathnameArr = pathname.split('/');
+   const posterName = pathnameArr[2];
+   const postTime = sectionNode.querySelector('time')?.getAttribute('datetime');
 
+   const action = target.className.includes('download-btn') ? 'download' : 'open';
 
-    const action = target.className.includes('download-btn') ? 'download' : 'open'
+   let mediaIndex = 0;
+   let url = null;
 
-    let mediaIndex = 0
-    let url = null
+   sectionNode.querySelector('header>div')?.childNodes.forEach((item, idx) => {
+      if (item instanceof HTMLDivElement && item.querySelector('div:nth-child(2)')?.classList.length === 2) {
+         mediaIndex = idx;
+      }
+   });
 
-    sectionNode.querySelector('header>div')?.childNodes.forEach((item, idx) => {
-        if (item instanceof HTMLDivElement && item.querySelector('div:nth-child(2)')?.classList.length === 2) {
-            mediaIndex = idx
-        }
-    })
-
-
-    if (pathname.startsWith('/stories/highlights/')) {
-        const id = "highlight:" + pathnameArr[3]
-        const res = await chrome.storage.local.get([id])
-        if (res[id]) {
-            const reels_media = res[id].reels_media[0]
-            const media = reels_media.items[mediaIndex]
-            if ('video_versions' in media) {
-                url = media['video_versions'][0].url
-            } else if ('image_versions2' in media) {
-                url = media['image_versions2'].candidates[0].url
+   if (pathname.startsWith('/stories/highlights/')) {
+      const id = 'highlight:' + pathnameArr[3];
+      const res = await chrome.storage.local.get([id]);
+      if (res[id]) {
+         const reels_media = res[id].reels_media[0];
+         const media = reels_media.items[mediaIndex];
+         if ('video_versions' in media) {
+            url = media['video_versions'][0].url;
+         } else if ('image_versions2' in media) {
+            url = media['image_versions2'].candidates[0].url;
+         }
+         if (url) {
+            const fileName = reels_media.user.username + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url);
+            if (action === 'download') {
+               downloadResource(url, fileName);
+            } else {
+               openInNewTab(url);
             }
-            if (url) {
-                const fileName = reels_media.user.username + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url)
-                if (action === 'download') {
-                    downloadResource(url, fileName);
-                } else {
-                    openInNewTab(url);
-                }
-                return
-            }
-        }
+            return;
+         }
+      }
+   }
 
-
-    }
-
-    url = await storyGetUrl(target, sectionNode);
-    if (url && url.length > 0) {
-        const fileName = posterName + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url)
-        if (action === 'download') {
-            downloadResource(url, fileName);
-        } else {
-            openInNewTab(url);
-        }
-    }
+   url = await storyGetUrl(target, sectionNode);
+   if (url && url.length > 0) {
+      const fileName = posterName + '-' + dayjs(postTime).format('YYYYMMDD_HHmmss') + '-' + getMediaName(url);
+      if (action === 'download') {
+         downloadResource(url, fileName);
+      } else {
+         openInNewTab(url);
+      }
+   }
 }
-
-
